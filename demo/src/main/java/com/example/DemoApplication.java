46a452e26cabcbf729d31e75c230109cd13abfde
yimi <yimi>
2016-10-10 10:38:49 +0800

    新增启动专属banner

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
